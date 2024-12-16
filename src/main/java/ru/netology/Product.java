6f67ef373fce095b00b249ee7cdb5ee311d4d900
Roman Leshin <vorzee@gmail.com>
2024-12-17 04:32:50 +0500

    Подготовили классы для задания

---
+++ src/main/java/ru/netology/Product.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Product {
+    protected final int id;
+    protected String title;
+    protected int price;
+
+    public Product(int id, String title, int price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
